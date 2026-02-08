81d1d125a7c9f71ee16f19b9474279b90547de59
saynarah <saynarah.nabuco@gmail.com>
2026-02-07 21:48:08 -0300

    second commit: project

---
+++ src/test/java/com/example/timeCapsule/TimeCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeCapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
